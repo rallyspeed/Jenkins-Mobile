08e28d92524d7452099eb3ef5421788a08b843cc
Mathieu Durand <28907836+rallyspeed@users.noreply.github.com>
2019-06-11 16:54:44 -0400

    Create runUAT.sh

---
+++ test/runUAT.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# set variables
+url=$1
+sleep_time=$2
+
+# wait for the app to start
+sleep ${sleep_time}
+
+# ping the app
+status_code=$(curl -LI "${url}" -w %{http_code} -o /dev/null -s)
+
+if [ $status_code == 200 ];
+then
+	echo "PASS: ${url} is reachable"
+else
+	echo "FAIL: ${url} is unreachable"
+    exit 1
+fi
